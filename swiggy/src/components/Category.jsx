013933669efb7027fb8a39f7d00d1df8dacf4a39
Manish Karne <manishkarne1967@gmail.com>
2025-02-25 18:41:21 +0530

    Update Category.jsx

--- swiggy/src/components/Category.jsx
+++ swiggy/src/components/Category.jsx
@@ -6,7 +6,7 @@ function Category() {
   const [categories, setCategory] = useState([]);
 
   const fetchCategory = async () => {
-    const response = await fetch("http://localhost:5000/categories");
+    const response = await fetch("https://swiggy-clone-app-backend-j6xi.onrender.com/categories");
     const data = await response.json();
     setCategory(data);
   };
