4d35b941395f1b5c3bd8575c680eb1884e2311d5
Manish Karne <manishkarne1967@gmail.com>
2025-02-25 18:33:36 +0530

    Update index.js

--- swiggy-api/index.js
+++ swiggy-api/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
